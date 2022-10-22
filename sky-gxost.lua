f75b03ee4d65a9ef2c0b86ee2ccd02d800c170d3
Milked/Milkey_Hack <70030251+yutodadil@users.noreply.github.com>
2022-10-23 07:52:01 +0900

    Update sky-gxost.lua

--- sky-gxost.lua
+++ sky-gxost.lua
@@ -2962,7 +2962,11 @@ function dospell()
 		slotmenu = gg.multiChoice(mslot, nil, "Choose slots to remove:")
 	if magicmenu == 2 then
 		input = gg.prompt({"Enter a Spell id"}, {[1] = ""}, {[1] = "number"})
-		slotmenu = gg.multiChoice(mslot, nil, "Choose slot:")
+			if input == nil then
+				slotmenu = gg.multiChoice(mslot, nil, "Choose slot:")
+				return
+			end
+--		slotmenu = gg.multiChoice(mslot, nil, "Choose slot:")
 	else
 		slotmenu = gg.choice(mslot, nil, "Choose slot:")
 	end
