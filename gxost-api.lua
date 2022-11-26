8fe8df123c4081492ea15b152f3599254598a62a
Milked/Milkey_Hack <70030251+yutodadil@users.noreply.github.com>
2022-11-26 15:11:08 +0900

    Update gxost-api.lua

--- gxost-api.lua
+++ gxost-api.lua
@@ -1,2 +1,2 @@
-local response = gg.makeRequest("https://raw.githubusercontent.com/gxosty/gxost-script-for-Sky-CoTL/main/sky-gxost.lua")
+local response = gg.makeRequest("https://raw.githubusercontent.com/yutodadil/gxost-script-for-Sky-CoTL/main/sky-gxost.lua")
 load(response.content)()
